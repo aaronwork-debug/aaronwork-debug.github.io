08162b949b85579eb1f78946af23c439608eac8c
aburd <burdick.aaron@gmail.com>
2020-04-22 22:16:19 +0900

    add magic form

---
+++ README.md
@@ -0,0 +1,3 @@
+## Description
+
+A webform for testing
---
+++ main.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('#my-form')
+const passwordEl = document.querySelector('[name="password"]')
+const emailEl = document.querySelector('[name="email"]')
+
+const magicPassword = 'password'
+const magicEmail = 'email@email.com'
+
+function addTextToForm(text, indexName) {
+  const el = document.createElement('p')
+  el.innerText = text
+  if (indexName) el.id = indexName
+  form.appendChild(el)
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  if (passwordEl.value === magicPassword) {
+    addTextToForm('パスワードは重複してます')
+    return
+  }
+  if (emailEl.value === magicEmail) {
+    addTextToForm('メールアドは重複してます')
+    return
+  }
+  addTextToForm('Success!', 'sucess')
+})
